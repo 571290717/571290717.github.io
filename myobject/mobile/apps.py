4a1a4072d40042dd51be79629e344074d8b449ab
571290717 <109459151+571290717@users.noreply.github.com>
2023-05-06 10:11:20 +0800

    Delete myobject directory

--- myobject/mobile/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MobileConfig(AppConfig):
-    name = 'mobile'
--- myobject/myadmin/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyadminConfig(AppConfig):
-    name = 'myadmin'
